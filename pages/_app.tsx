95fc49f13273dc5db47a5b2d9e6f719514ea7347
liangshuo <liangshuoone@aliyun.com>
2022-12-11 17:18:42 +0800

    feat: add themes css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
+import "../styles/vars.css";
 import type { AppProps } from "next/app";
 
+
+// root
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
