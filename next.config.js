f983565e0f83d5684497085bf1a2754621985829
liangshuo <liangshuoone@aliyun.com>
2022-12-11 17:17:58 +0800

    chore: update svg config

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  }
 };
 
 module.exports = nextConfig;
