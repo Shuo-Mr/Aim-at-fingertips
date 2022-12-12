ff10a960a22be4068cc0135a656f1ee61d0e5f64
liangshuo <liangshuoone@aliyun.com>
2022-12-12 10:17:26 +0800

    feat: add @svgr

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config 
   }
 };
 
